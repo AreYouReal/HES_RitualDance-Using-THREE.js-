47a67dcf1270536c914d4e4963094f4324e25685
Alexander <viertual@gmail.com>
2013-10-21 19:06:33 +0400

    Almost done?

--- Alexander/rdance.js
+++ Alexander/rdance.js
@@ -6,7 +6,8 @@ var FOVY = 45, ASPECT = WIDTH / HEIGHT,
 var renderer;
 var camera;
 var scene;
-var sphere, plane;
+var sphere, plane, coin;
+var lightTurnsOn = true;
 var pointLight, pointLight_2;
 var sphereMatrix;
 var rotateAngle = 0.1;
@@ -27,7 +28,6 @@ var secondRoboRotor = new THREE.Object3D();
 window.onload = function init(){
 
     var $container = $('#container');
-    console.log(container);
     // Create a WebGL renderer object,
     // camera and scene
     renderer = new THREE.WebGLRenderer();
@@ -48,9 +48,7 @@ window.onload = function init(){
     sphere = createSphere(50, 16, 16);
     sphere.position.set(-200, 300, 0);
     plane = createPlane();
-
-
-
+    coin = createCoin();
 
     cubicMan = createCubicMan();
     flyingRobot = createFlyingRobot();
@@ -60,14 +58,14 @@ window.onload = function init(){
     scene.add(sphere);
     scene.add(cubicMan);
     scene.add(flyingRobot);
+    scene.add(coin);
     // add the camera to the scene
     scene.add(camera);
 
     pointLight = createPointLight(-200, 300, 200);
-
     pointLight_2 = createPointLight(0, 0, 0);
 
-    //scene.add(pointLight);
+    scene.add(pointLight);
     scene.add(pointLight_2);
 
     document.onkeydown = function(){keyCatch(window.event.keyCode)};
@@ -77,19 +75,21 @@ window.onload = function init(){
     draw();
 };
 
-function createCube(){
+// Creates white cube
+function createCube(width, height, depth){
     var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
-    return new THREE.Mesh(new THREE.CubeGeometry(100, 100, 50), cubeMaterial);
+    return new THREE.Mesh(new THREE.CubeGeometry(width, height, depth), cubeMaterial);
 }
 
-
+// Creates white sphere
 function createSphere(radius, segments, rings){
     sphereMatrix = new THREE.Matrix4();
     sphereMatrix.makeRotationX(rotateAngle);
-    var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xCC0000});
+    var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xFDFDAD});
     return new THREE.Mesh(new THREE.SphereGeometry(radius, segments, rings), sphereMaterial);
 }
 
+// Creates white point light
 function createPointLight(x, y, z){
     var pointLight = new THREE.PointLight(0xFFFFFF);
     pointLight.position.x = x;
@@ -133,7 +133,6 @@ function update(){
         limbAngle -= 0.1;
     }
 
-
     cubicManPosition[0] = 200 * Math.cos(circleAngle);
     cubicManPosition[1] = 200 * Math.sin(circleAngle);
     circleAngle -= 0.1;
@@ -151,12 +150,14 @@ function update(){
     pointLight_2.position.x = flyingRobot.position.x;
     pointLight_2.position.z = flyingRobot.position.z;
     pointLight_2.position.y = flyingRobot.position.y;
+
+    coin.rotateZ(5 * rotateAngle);
 }
 
 // Create cubicMan
 function createCubicMan(){
     var cubeMan = new THREE.Object3D();
-    cubeMan.add(createCube());
+    cubeMan.add(createCube(100, 100, 50));
     leftHand = createHand(true);
     cubeMan.add(leftHand);
     rightHand = createHand(false);
@@ -175,7 +176,7 @@ function createCubicMan(){
 // Creates cubicMan hands (left and right)
 function createHand(left){
     var hand = new THREE.Object3D();
-    var tempCube = createCube();
+    var tempCube = createCube(100, 100, 50);
     if(left)
         tempCube.position.x = -60;
     else
@@ -188,7 +189,7 @@ function createHand(left){
 // Creates cubicMan legs (left and right)
 function createLeg(left){
     var leg = new THREE.Object3D();
-    var tempCube = createCube();
+    var tempCube = createCube(100, 100, 50);
     if(left)
         tempCube.position.x = -25;
     else
@@ -201,26 +202,23 @@ function createLeg(left){
 
 // Creates plane cubicMan running on.
 function createPlane(){
-    var planeTexture = new THREE.ImageUtils.loadTexture('/roughgrass1.jpg');
+    var planeTexture = new THREE.ImageUtils.loadTexture('roughgrass1.jpg');
     var planeMaterial = new THREE.MeshLambertMaterial({color: 0x334411});
     planeTexture.wrapT = planeTexture.wrapS = THREE.RepeatWrapping;
     planeTexture.repeat.set(10, 10);
     //var planeMaterial = new THREE.MeshBasicMaterial({map: planeTexture});
     var tempPlane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), planeMaterial);
-    var planeMatrix = new THREE.Matrix4();
-    planeMatrix.makeScale(5, 5, 1);
-    planeMatrix.makeRotationX(- Math.PI / 4);
-    tempPlane.position.y = - 200;
+    tempPlane.position.y = - 150;
     tempPlane.rotateX(-Math.PI / 2);
-    tempPlane.scale.set(20, 20, 1);
+    tempPlane.scale.set(50, 50, 1);
     return tempPlane;
 }
 
+// Creates robot which is flying around
 function createFlyingRobot(){
     var bladeMaterial = new THREE.MeshLambertMaterial({color: 0xCC00FF});
     var sphere = new THREE.Mesh(new THREE.SphereGeometry(20, 16, 16), bladeMaterial);
     roboRotor.add(sphere);
-
     for(var i = 0; i < 6; i++){
         var blade = new THREE.Mesh(new THREE.CubeGeometry(20, 5, 115), bladeMaterial);
         blade.rotateY(Math.PI * 2 / 6 * i);
@@ -228,48 +226,56 @@ function createFlyingRobot(){
         blade.position.x = 75 * Math.sin(Math.PI * 2 / 6 * i);
         roboRotor.add(blade);
     }
-
     secondRoboRotor = roboRotor.clone();
     secondRoboRotor.position.y = - 75;
-
     var cylinder = new THREE.Mesh(new THREE.CylinderGeometry(10, 10, 150), bladeMaterial);
     cylinder.position.y = -75;
-
     var bulbMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF, opacity: 1});
     bulbMaterial.emissive = new THREE.Color( 0xCC00ff );
     var bulb = createSphere(50, 16, 16);
     bulb.position.y = -175;
     bulb.material = bulbMaterial;
-
     var robot = new THREE.Object3D();
     robot.add(roboRotor);
     robot.add(cylinder);
     robot.add(secondRoboRotor);
     robot.add(bulb);
-
     robot.position.y = 500;
     return robot;
 }
 
+// Create coin in the center of the scene
+function createCoin(){
+    var coneMaterial = new THREE.MeshLambertMaterial({color: 0xFFFF00});
+    var tempCoin = new THREE.Mesh(new THREE.CylinderGeometry(120, 120, 10, 32), coneMaterial);
+    tempCoin.position.y = - 30;
+    tempCoin.rotateX(Math.PI / 2);
+    return tempCoin;
+}
 
-
+// Catch keyboar events
 function keyCatch(keyCode){
     switch(window.event.keyCode){
         case 37:
-            console.log("left");
-            camera.position.x -= 10;
+            camera.position.x -= 50;
             break;
         case 38:
-            console.log("up");
-            camera.position.y += 10;
+            camera.position.z -= 50;
             break;
         case 39:
-            console.log("right");
-            camera.position.x += 10;
+            camera.position.x += 50;
             break;
         case 40:
-            console.log("down");
-            camera.position.y -= 10;
+            camera.position.z += 50;
+            break;
+        case 76:
+            if(lightTurnsOn){
+                scene.remove(pointLight);
+                lightTurnsOn = false;
+            }else{
+                scene.add(pointLight);
+                lightTurnsOn = true;
+            }
             break;
     }
 }
