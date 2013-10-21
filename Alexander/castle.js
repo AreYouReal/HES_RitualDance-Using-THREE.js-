5d095710a0c9b70965929b783aa7140a98722d9d
Alexander Kolesnikov <AreYouReal@dreamindustries.co>
2013-10-21 12:14:11 +0400

    Plane and textures were added.

--- Alexander/castle.js
+++ Alexander/castle.js
@@ -1,15 +1,17 @@
 
-var WIDTH = 400, HEIGHT = 300;
+var WIDTH = 1024, HEIGHT = 768;
 var FOVY = 45, ASPECT = WIDTH / HEIGHT,
     NEAR = 0.1, FAR = 10000;
 
 var renderer;
 var camera;
 var scene;
-var sphere;
+var sphere, cube, plane;
 var pointLight;
 var sphereMatrix;
-var rotateAngle = 0;
+var rotateAngle = 0.1;
+
+var cubicMan;
 
 window.onload = function init(){
 
@@ -22,7 +24,7 @@ window.onload = function init(){
     scene = new THREE.Scene();
 
     // camera starts at [0, 0, 0] so move it
-    camera.position.z = 300;
+    camera.position.z = 1000;
 
     // start the renderer
     renderer.setSize(WIDTH, HEIGHT);
@@ -32,9 +34,28 @@ window.onload = function init(){
 
     // create sphere's material
     sphere = createSphere();
+    cube = createCube();
+    cube.position.x = 150;
+    plane = createPlane();
+
+    var planeMatrix = new THREE.Matrix4();
+    planeMatrix.makeScale(5, 5, 1);
+    planeMatrix.makeRotationX(- Math.PI / 4);
+
+    plane.position.y = - 100;
+    plane.rotateX(-Math.PI / 2);
+    plane.scale.set(10, 10, 1);
+
+
+
+    cubicMan = createCubicMan();
+
 
     // add the sphere to the scene
+    scene.add(plane);
     scene.add(sphere);
+    scene.add(cube);
+    scene.add(cubicMan);
     // add the camera to the scene
     scene.add(camera);
 
@@ -43,13 +64,18 @@ window.onload = function init(){
     scene.add(pointLight);
 
     // draw!
+    update();
     draw();
 };
 
+function createCube(){
+    var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xFFFF00});
+    return new THREE.Mesh(new THREE.CubeGeometry(100, 100, 100), cubeMaterial);
+}
 
 function createSphere(){
     sphereMatrix = new THREE.Matrix4();
-    sphereMatrix.rotateX(rotateAngle);
+    sphereMatrix.makeRotationX(rotateAngle);
 
     var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xCC0000});
     var radius = 50, segments = 16, rings = 16;
@@ -67,13 +93,35 @@ function createPointLight(x, y, z){
 function draw(){
     renderer.render(scene, camera);
     setTimeout(function(){requestAnimationFrame(draw)}, 100);
-    sphereMatrix.rotateX(rotateAngle += 10);
-    if(rotateAngle > 360)
-        rotateAngle = 0;
-    sphere.matrix = sphereMatrix;
-    sphere.applyMatrix(sphere.matrix);
-    console.log(sphere.matrix);
 }
+
+function update(){
+    setTimeout(function(){requestAnimationFrame(update)}, 50);
+    sphere.rotateX(rotateAngle);
+    cube.rotateY(rotateAngle);
+    cubicMan.rotateZ(rotateAngle);
+}
+
+
+function createCubicMan(){
+    var cubeMan = new THREE.Object3D();
+    cubeMan.add(createCube());
+    cubeMan.position.x = - 150;
+    return cubeMan;
+}
+
+function createPlane(){
+    var planeTexture = new THREE.ImageUtils.loadTexture('roughgrass1.jpg');
+    planeTexture.wrapT = planeTexture.wrapS = THREE.RepeatWrapping;
+    planeTexture.repeat.set(10, 10);
+    var planeMaterial = new THREE.MeshBasicMaterial({map: planeTexture});
+    return new THREE.Mesh(new THREE.PlaneGeometry(100, 100), planeMaterial);
+}
+
+
+
+
+
 
 
 
